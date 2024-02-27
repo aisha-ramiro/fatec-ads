59fca71883d359d066181a5dd8d0880e3672b616
Aisha Ramiro <79142932+aisha-ramiro@users.noreply.github.com>
2024-02-26 21:38:31 -0300

    atividade 4 - aula 2

---
+++ algoritmo-e-logica-de-programacao/aula2/atv4.py
@@ -0,0 +1,8 @@
+aluno = input("Digite o nome do aluno: ")
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+print(f"A média final do aluno(a) {aluno} é: {media:.1f}")
